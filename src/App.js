310d58450d932f955116d999c09a7b9a739e3e35
Sheersho Zaman <sheersho.zaman@protonmail.com>
2024-01-27 23:08:56 +0600

    A random quote generator
    A page that displays a random quote, and can generate a random quote whenever the "New Quote" button is pressed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [quoteInfo, setQuoteInfo] = useState({});
+
+  useEffect(() => {
+    getQuote();
+    setColor();
+  }, []);
+
+  const getQuote = () => {
+    fetch("https://api.quotable.io/random")
+    .then((res) => res.json())
+    .then((data) => {
+      setQuoteInfo({
+        text: data.content,
+        author: data.author
+      });
+    })
+    setColor();
+  };
+
+  const setColor = () => {
+    const color = getColor();
+    document.querySelector(".App").style.backgroundColor = color;
+    document.getElementById("text").style.color = color;
+    document.getElementById("author").style.color = color;
+    document.getElementById("new-quote").style.backgroundColor = color;
+  }
+
+  const getColor = () => {
+    let color = "#";
+    const hexaArr =  ['5198a8','5165a8','7551a8','cf5353','cf9953','68b55b','5bb579','375c59'];
+    color += hexaArr[Math.floor(Math.random() * hexaArr.length)];
+    return color;
+  }
+
+  
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div id="quote-box" style={{backgroundColor: "white"}}>
+        <div id="text">{quoteInfo.text}</div>
+        <div id="author">{"-" + quoteInfo.author}</div>
+        <button id="new-quote" onClick={getQuote}>New Quote</button>
+        <a id="tweet-quote" href={'https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text' + quoteInfo.text}>Post to X</a>
+      </div>
     </div>
   );
 }
